80b3bd55a2b4ecc4a8016bc8ae8efafe1b118d0c
SYEDUMAIR007 <92495743+SYEDUMAIR007@users.noreply.github.com>
2023-03-12 19:47:57 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Frontend Mentor - Calculator app solution
+
+This is a solution to the [Calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+  - [Author](#author)
+
+
+
+## Overview
+This challenge was TRICKY, creating the toggle button was a challenging task and i didnt had the figma or sketch files but im happy that i could make it as realistic as possible.
+### The challenge
+
+Users should be able to:
+
+- See the size of the elements adjust based on their device's screen size
+- Perform mathmatical operations like addition, subtraction, multiplication, and division
+- Adjust the color theme based on their preference
+
+
+## My process
+I first went on to design my own mockup of the design using figma and then built the project.
+
+### Built with
+
+- Semantic HTML5 markup
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Java script
+
+
+### What I learned
+I understood vanilla js very well while creating this project.
+
+
+### Useful resources
+
+- [Figma](https://www.figma.com) - This helped me for buildin the mockup . I really liked this pattern of building mockups first and will use it going forward.
+- [MDN web docs](https://developer.mozilla.org/en-US/) - This is an amazing website which helped me finally understand the vanilla js. 
+
+
+## Author
+
+- Frontend Mentor -[@SYEDUMAIR007](https://www.frontendmentor.io/profile/SYEDUMAIR007)
+
+
+
---
+++ app.js
@@ -0,0 +1,249 @@
+const inputScreen=document.querySelector("#inputScreen");
+const numbers=document.querySelectorAll(".numbers");
+const operators=document.querySelectorAll(".operators");
+const equalbtn=document.querySelector("#equalbtn");
+const resetbtn=document.querySelector("#resetbtn");
+const deletebtn=document.querySelector("#deletebtn");
+const percentagebtn=document.querySelector("#percentagebtn");
+const funcbtn=document.querySelectorAll(".funcButtons");
+const allButtons=document.querySelectorAll("button");
+const endbtn=document.querySelectorAll(".endbtn");
+//styling
+const header=document.querySelector("header");
+const buttonContainer=document.querySelector(".buttonHolder");
+const themeChanger=document.querySelector("#outerContainer");
+const themeSwitchContainer=document.querySelector("#buttonContainer");
+const themeSwitchText=document.querySelector("legendTextContainer");
+const toggleSwitch=document.querySelector(".redButton");
+const blueMode=document.querySelector("#bluemode");
+const lightMode=document.querySelector("#lightmode");
+const violetMode=document.querySelector("#violetmode");
+let currentInput =0;
+let firstNumber = null;
+let operator = null;
+
+inputScreen.value=0;
+numbers.forEach(number=>{
+    number.addEventListener('click',handleNumberButtonClick);
+})
+operators.forEach(operator=>{
+    operator.addEventListener('click',handleOperatorButtonClick);
+})
+equalbtn.addEventListener('click',handleEqualButtonClick);
+percentagebtn.addEventListener('click',handlePercentButtonClick);
+resetbtn.addEventListener('click',reset);
+deletebtn.addEventListener('click',deleting);
+blueMode.addEventListener('click',go_to_1);
+lightMode.addEventListener('click',go_to_2);
+violetMode.addEventListener('click',go_to_3);
+
+
+        function go_to_1() {
+            document.body.style.backgroundColor="hsl(222, 26%, 31%)";
+            toggleSwitch.style.backgroundColor="hsl(6, 63%, 50%)";
+            inputScreen.style.backgroundColor="hsl(224, 36%, 15%)";
+            inputScreen.style.color="hsl(30, 25%, 89%)";
+            themeSwitchContainer.style.backgroundColor="hsl(223, 31%, 20%)";
+            header.style.color="hsl(0, 0%, 100%)";
+            buttonContainer.style.backgroundColor="hsl(223, 31%, 20%)";
+            toggleSwitch.classList.add('horizTranslate1');
+            toggleSwitch.classList.remove('horizTranslate2');
+            toggleSwitch.classList.remove('horizTranslate3');
+            allButtons.forEach(button=>{
+              button.classList.add('bluetheme-allbtn');
+              button.classList.remove('lighttheme-allbtn');
+              button.classList.remove('violettheme-allbtn');
+            });
+            funcbtn.forEach(button=>{
+              button.classList.add('bluetheme-funcbtn');
+              button.classList.remove('lighttheme-funcbtn');
+              button.classList.remove('violettheme-funcbtn');
+            });
+            endbtn.forEach(button=>{
+              button.classList.add('bluetheme-endbtn');
+              button.classList.remove('lighttheme-endbtn');
+              button.classList.remove('violettheme-endbtn');
+            });
+        }
+
+        function go_to_2() {
+            document.body.style.backgroundColor="hsl(0, 0%, 90%)";
+            toggleSwitch.style.backgroundColor="hsl(25, 98%, 40%)";
+            inputScreen.style.backgroundColor="hsl(0, 0%, 93%)";
+            inputScreen.style.color="hsl(198, 20%, 13%)";
+            themeSwitchContainer.style.backgroundColor="hsl(0, 5%, 81%)";
+            header.style.color="hsl(198, 20%, 13%)";
+            buttonContainer.style.backgroundColor="hsl(0, 5%, 81%)";
+            toggleSwitch.classList.add('horizTranslate2');
+            toggleSwitch.classList.remove('horizTranslate3');
+            toggleSwitch.classList.remove('horizTranslate1');
+            allButtons.forEach(button=>{
+              button.classList.add('lighttheme-allbtn');
+              button.classList.remove('bluetheme-allbtn');
+              button.classList.remove('violettheme-allbtn');
+            });
+            funcbtn.forEach(button=>{
+              button.classList.add('lighttheme-funcbtn');
+              button.classList.remove('bluetheme-funcbtn');
+              button.classList.remove('violettheme-funcbtn');
+            });
+            endbtn.forEach(button=>{
+              button.classList.add('lighttheme-endbtn');
+              button.classList.remove('bluetheme-endbtn');
+              button.classList.remove('violettheme-endbtn');
+            });
+        }
+
+        function go_to_3() {
+            document.body.style.backgroundColor="hsl(268, 75%, 9%)";
+            toggleSwitch.style.backgroundColor="hsl(176, 100%, 44%)";
+            inputScreen.style.backgroundColor="hsl(268, 71%, 12%)";
+            themeSwitchContainer.style.backgroundColor="hsl(268, 71%, 12%)";
+            inputScreen.style.color="hsl(52, 100%, 62%)";
+            header.style.color="hsl(52, 100%, 62%)";
+            buttonContainer.style.backgroundColor="hsl(268, 71%, 12%)";
+            toggleSwitch.classList.add('horizTranslate3');
+            toggleSwitch.classList.remove('horizTranslate2');
+            toggleSwitch.classList.remove('horizTranslate1');
+            allButtons.forEach(button=>{
+              button.classList.add('violettheme-allbtn');
+              button.classList.remove('bluetheme-allbtn');
+              button.classList.remove('lighttheme-allbtn');
+            });
+            funcbtn.forEach(button=>{
+              button.classList.add('violettheme-funcbtn');
+              button.classList.remove('bluetheme-funcbtn');
+              button.classList.remove('lighttheme-funcbtn');
+            });
+            endbtn.forEach(button=>{
+              button.classList.add('violettheme-endbtn');
+              button.classList.remove('bluetheme-endbtn');
+              button.classList.remove('lighttheme-endbtn');
+            });
+        }
+
+function handleNumberButtonClick(e) {
+    const buttonValue = e.target.value;
+    currentInput += buttonValue;
+    inputScreen.value = currentInput;
+  }
+
+  function handleOperatorButtonClick(e) {
+    if (firstNumber === null) {
+      const buttonValue = e.target.value;
+      firstNumber = parseFloat(currentInput);
+      operator = buttonValue;
+      inputScreen.value += operator;
+      currentInput = '';
+    }else if(currentInput=='' && firstNumber!=null){
+        operator=e.target.value;
+        inputScreen.value=firstNumber+ operator;
+    } 
+    else{
+        currentInput = parseFloat(currentInput);
+        switch (operator) {
+          case '+':
+            firstNumber += currentInput;
+            inputScreen.value = firstNumber;
+            operator = e.target.value; 
+            inputScreen.value +=operator;
+            break;
+          case '-':
+                  firstNumber -= currentInput;
+                  inputScreen.value = firstNumber;
+                  operator = e.target.value; 
+                  inputScreen.value +=operator;
+            break;
+          case '/':
+                  if(currentInput!=0){
+                  firstNumber /= currentInput;
+                  inputScreen.value = firstNumber;
+                  operator = e.target.value; 
+                  inputScreen.value +=operator;
+                }
+                  else{
+                    firstNumber=null;
+                    currentInput='';
+                    operator='';
+                    inputScreen.value="not valid operation"+operator;
+                  }
+            break;
+          case '*':
+                  firstNumber *= currentInput;
+                  inputScreen.value = firstNumber;
+                  operator = e.target.value; 
+                  inputScreen.value +=operator;
+            break;
+        }
+        currentInput = '';
+    }
+  }
+  function handleEqualButtonClick(e) {
+   
+    if (operator !== null) {
+      currentInput = parseFloat(currentInput);
+      equals(operator);
+      currentInput = '';
+    }
+  }
+function equals(operator){
+    switch (operator) {
+        case '+':
+          firstNumber += currentInput;
+          inputScreen.value = firstNumber;
+          operator = e.target.value; 
+          inputScreen.value +=operator;
+          break;
+        case '-':
+                firstNumber -= currentInput;
+                inputScreen.value = firstNumber;
+                operator = e.target.value; 
+                inputScreen.value +=operator;
+          break;
+        case '/':
+                if(currentInput!=0){
+                firstNumber /= currentInput;
+                inputScreen.value = firstNumber;
+                operator = e.target.value; 
+                inputScreen.value +=operator;
+              }
+                else{
+                  firstNumber=null;
+                  currentInput='';
+                  operator='';
+                  inputScreen.value="not valid operation"+operator;
+                }
+          break;
+        case '*':
+                firstNumber *= currentInput;
+                inputScreen.value = firstNumber;
+                operator = e.target.value; 
+                inputScreen.value +=operator;
+          break;
+      }
+  
+      
+}
+function handlePercentButtonClick(){
+   firstNumber= parseFloat(inputScreen.value)/100;
+  
+   inputScreen.value=firstNumber;
+}
+
+function reset(){
+    inputScreen.value=0;
+    currentInput='';
+    firstNumber=null;
+    operator=null;
+}
+function deleting(){
+   currentInput= currentInput.slice(0,-1)
+   if(currentInput=='')
+    inputScreen.value=0;
+    else
+    inputScreen.value=currentInput
+   
+}
+   
+
+
